373391c057aec496b52ec49e182861843c797b0f
SkinnyGuille <guillermo.n.rosales@gmail.com>
2023-08-31 16:30:55 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/practicon5/PracticoN5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practicon5;
+
+/**
+ *
+ * @author Familia
+ */
+public class PracticoN5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
